c5e6f0b18166bfc579fabd5186a450ae5829ea81
Daniel Lopes <daniel.lopes.amorim@hotmail.com>
2025-02-18 22:19:46 -0300

    implement deputado participation retrieval and display

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
-	"api-gov/service"
 	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/Danielopes7/camara-deputados-stats/service"
+	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
@@ -12,11 +17,55 @@ func main() {
 		fmt.Println("Erro ao buscar dados:", error)
 		return
 	}
-	for _, deputado := range data {
-		fmt.Printf("Nome: %d\n", deputado.ID)
+	getParticipation(service.Deputados{Dados: data})
+	fmt.Printf("Encontrados %d deputados\n", len(data))
+}
+
+func getParticipation(data service.Deputados) {
+
+	for index, deputado := range data.Dados {
+		if index > 5 {
+			break
+		}
+		url := fmt.Sprintf("https://www.camara.leg.br/deputados/%d?ano=2024", deputado.ID)
+
+		res, err := http.Get(url)
+		if err != nil {
+			log.Fatal("Erro ao acessar a página:", err)
+		}
+
+		if res.StatusCode != 200 {
+			log.Fatalf("Erro ao acessar a página: Status %d", res.StatusCode)
+		}
+
+		doc, err := goquery.NewDocumentFromReader(res.Body)
+		if err != nil {
+			log.Fatal("Erro ao processar HTML:", err)
+		}
+
+		presencas := make(map[string]string)
+
+		doc.Find(".presencas__section").Each(func(i int, s *goquery.Selection) {
+			s.Find(".presencas__data.presencas__mobile-outras").Each(func(j int, item *goquery.Selection) {
+				label := strings.TrimSpace(item.Find(".presencas__label").Text())
+				valor := strings.TrimSpace(item.Find(".presencas__qtd").Text())
+				chave := fmt.Sprintf("[%d] -   %s", i, label)
+
+				presencas[chave] = valor
+			})
+		})
+
+		fmt.Printf("ID: %d\n", deputado.ID)
+		fmt.Printf("Nome: %s\n", deputado.Nome)
 		fmt.Printf("Partido: %s\n", deputado.SiglaPartido)
 		fmt.Printf("UF: %s\n", deputado.SiglaUf)
-		fmt.Println("---------------")
+
+		fmt.Println("📊 Presença e Ausência do Deputado:")
+		for chave, valor := range presencas {
+			fmt.Printf("%s: %s\n", chave, valor)
+		}
+		fmt.Println("------------------------------------------------------------")
+
 	}
-	fmt.Printf("Encontrados %d deputados\n", len(data))
+
 }
